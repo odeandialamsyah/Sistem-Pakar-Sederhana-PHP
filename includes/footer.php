13fa740d407e276ce32040dedc1702da6243082d
odeandialamsyah <odeandialamsyah@gmail.com>
2024-07-04 19:54:13 +0700

    includes

---
+++ includes/footer.php
@@ -0,0 +1,5 @@
+<footer>
+        <p>&copy; 2024 Sistem Pakar. All rights reserved.</p>
+    </footer>
+</body>
+</html>
---
+++ includes/header.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Sistem Pakar</title>
+    <link rel="stylesheet" href="assets/style.css">
+</head>
+<body>
+    <header>
+        <h1>Sistem Pakar Diagnosa Penyakit</h1>
+    </header>
+    <nav>
+        <ul>
+            <li><a href="index.php">Beranda</a></li>
+            <li><a href="input_gejala.php">Input Gejala</a></li>
+            <li><a href="admin/manajemen_data.php">Manajemen Data</a></li>
+        </ul>
+    </nav>
