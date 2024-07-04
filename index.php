dc205ded94a7340c33439a590fedb16fc7375533
odeandialamsyah <odeandialamsyah@gmail.com>
2024-07-04 19:54:45 +0700

    index

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+include 'includes/header.php';
+?>
+<div class="container">
+    <h1>Selamat Datang di Sistem Pakar Diagnosa Penyakit</h1>
+    <p>Gunakan sistem ini untuk mendiagnosa penyakit berdasarkan gejala yang Anda alami. Ikuti petunjuk yang ada di setiap halaman untuk menggunakan sistem ini.</p>
+</div>
+<?php
+include 'includes/footer.php';
+?>
